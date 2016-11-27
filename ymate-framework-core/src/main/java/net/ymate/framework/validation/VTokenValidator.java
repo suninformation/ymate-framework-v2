244e908aa0a96167b1b46fae880109514e1d9592
suninformation <suninformation@163.com>
2016-11-27 12:38:36 +0800

    新增Token验证注解及验证器

---
+++ ymate-framework-core/src/main/java/net/ymate/framework/validation/VTokenValidator.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2007-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.ymate.framework.validation;
+
+import net.ymate.framework.core.support.TokenProcessHelper;
+import net.ymate.platform.core.beans.annotation.CleanProxy;
+import net.ymate.platform.core.i18n.I18N;
+import net.ymate.platform.validation.IValidator;
+import net.ymate.platform.validation.ValidateContext;
+import net.ymate.platform.validation.ValidateResult;
+import net.ymate.platform.validation.annotation.Validator;
+import net.ymate.platform.webmvc.context.WebContext;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author 刘镇 (suninformation@163.com) on 16/11/27 下午12:17
+ * @version 1.0
+ */
+@Validator(VToken.class)
+@CleanProxy
+public class VTokenValidator implements IValidator {
+
+    public ValidateResult validate(ValidateContext context) {
+        boolean _matched = false;
+        VToken _vToken = (VToken) context.getAnnotation();
+        if (context.getParamValue() != null) {
+            if (!TokenProcessHelper.getInstance().isTokenValid(WebContext.getRequest(), _vToken.name(), _vToken.reset())) {
+                _matched = true;
+            }
+        }
+        if (_matched) {
+            String _pName = StringUtils.defaultIfBlank(context.getParamLabel(), context.getParamName());
+            _pName = I18N.formatMessage(VALIDATION_I18N_RESOURCE, _pName, _pName);
+            String _msg = StringUtils.trimToNull(_vToken.msg());
+            if (_msg != null) {
+                _msg = I18N.formatMessage(VALIDATION_I18N_RESOURCE, _msg, _msg, _pName);
+            } else {
+                _msg = I18N.formatMessage(VALIDATION_I18N_RESOURCE, "ymp.validation.token_invalid", "{0} is invalid.", _pName);
+            }
+            return new ValidateResult(context.getParamName(), _msg);
+        }
+        return null;
+    }
+}
