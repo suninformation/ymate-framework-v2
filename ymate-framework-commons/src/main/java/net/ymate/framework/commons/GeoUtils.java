36b54d397787247cb36e0a4372c89f031639e68f
suninformation <suninformation@163.com>
2018-03-31 14:25:45 +0800

    优化GeoUtils并新增针对多边形和圆形区域判断相关方法

--- ymate-framework-commons/src/main/java/net/ymate/framework/commons/GeoUtils.java
+++ ymate-framework-commons/src/main/java/net/ymate/framework/commons/GeoUtils.java
@@ -15,9 +15,17 @@
  */
 package net.ymate.framework.commons;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.NullArgumentException;
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.awt.geom.Point2D;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author 刘镇 (suninformation@163.com) on 16/12/12 下午3:27
@@ -44,9 +52,7 @@ public class GeoUtils {
      * @return 计算两点间的距离(米)
      */
     public static double distance(Point p1, Point p2) {
-        double _lat1 = __rad(p1.getLatitude());
-        double _lat2 = __rad(p2.getLatitude());
-        return Math.round(2 * Math.asin(Math.sqrt(Math.pow(Math.sin((_lat1 - _lat2) / 2), 2) + Math.cos(_lat1) * Math.cos(_lat2) * Math.pow(Math.sin((__rad(p1.getLongitude()) - __rad(p2.getLongitude())) / 2), 2))) * EARTH_RADIUS * 10000) / 10;
+        return p1.distance(p2);
     }
 
     /**
@@ -75,20 +81,57 @@ public class GeoUtils {
     }
 
     /**
+     * 判断点是否在多边形区域内
+     *
+     * @param polygon 多边形区域
+     * @param point   待判断点
+     * @return true - 多边形包含这个点, false - 多边形未包含这个点。
+     */
+    public static boolean contains(Polygon polygon, Point point) {
+        return contains(polygon, point, false);
+    }
+
+    public static boolean contains(Polygon polygon, Point point, boolean on) {
+        if (on) {
+            // 判断是否在多边形区域边界上
+            return polygon.on(point);
+        }
+        // 判断点是否在多边形区域内
+        return polygon.in(point);
+    }
+
+    /**
+     * 判断点是否在圆形范围内
+     *
+     * @param circle 圆形区域
+     * @param point  待判断点
+     * @return -1 - 点在圆外, 0 - 点在圆上, 1 - 点在圆内
+     */
+    public static int contains(Circle circle, Point point) {
+        return circle.contains(point);
+    }
+
+    /**
      * 地理坐标点
      */
-    public static class Point {
+    public static class Point implements Serializable {
 
         /**
-         * 经度
+         * 经度, X
          */
         private double longitude;
 
         /**
-         * 纬度
+         * 纬度, Y
          */
         private double latitude;
 
+        /**
+         * 构造器
+         *
+         * @param longitude 经度
+         * @param latitude  纬度
+         */
         public Point(double longitude, double latitude) {
             this.longitude = longitude;
             this.latitude = latitude;
@@ -108,6 +151,32 @@ public class GeoUtils {
 
         public void setLatitude(double latitude) {
             this.latitude = latitude;
+        }
+
+        public Point2D.Double toPoint2D() {
+            return new Point2D.Double(longitude, latitude);
+        }
+
+        /**
+         * @param point 坐标点
+         * @return 计算两点间的距离(米)
+         */
+        public double distance(Point point) {
+            double _lat1 = __rad(latitude);
+            double _lat2 = __rad(latitude);
+            return Math.round(2 * Math.asin(Math.sqrt(Math.pow(Math.sin((_lat1 - _lat2) / 2), 2) + Math.cos(_lat1) * Math.cos(_lat2) * Math.pow(Math.sin((__rad(point.getLongitude()) - __rad(point.getLongitude())) / 2), 2))) * EARTH_RADIUS * 10000) / 10;
+        }
+
+        /**
+         * @return 验证是否为合法有效的经纬度
+         */
+        public boolean isValidCoordinate() {
+            // 经度: 180° >= x >= 0°
+            if (0.0 > longitude || 180.0 < longitude) {
+                return false;
+            }
+            // 纬度: 90° >= y >= 0°
+            return !(0.0 > latitude) && !(90.0 < latitude);
         }
 
         @Override
@@ -142,7 +211,7 @@ public class GeoUtils {
     /**
      * 地理坐标矩形区域
      */
-    public static class Bounds {
+    public static class Bounds implements Serializable {
 
         /**
          * 左下(西南)角坐标点
@@ -214,7 +283,7 @@ public class GeoUtils {
          * @param point 地理坐标点
          * @return 地理坐标点是否位于此矩形内
          */
-        public boolean containsPoint(Point point) {
+        public boolean contains(Point point) {
             return !isEmpty() && (point.getLongitude() >= southWest.getLongitude() && point.getLongitude() <= northEast.getLongitude()) && (point.getLatitude() >= southWest.getLatitude() && point.getLatitude() <= northEast.getLatitude());
         }
 
@@ -222,8 +291,8 @@ public class GeoUtils {
          * @param bounds 矩形区域
          * @return 矩形区域是否完全包含于此矩形区域中
          */
-        public boolean containsBounds(Bounds bounds) {
-            return containsPoint(bounds.getSouthWest()) && containsPoint(bounds.getNorthEast());
+        public boolean contains(Bounds bounds) {
+            return contains(bounds.getSouthWest()) && contains(bounds.getNorthEast());
         }
 
         /**
@@ -273,6 +342,189 @@ public class GeoUtils {
         @Override
         public String toString() {
             return "Bounds{southWest=" + southWest + ", northEast=" + northEast + '}';
+        }
+    }
+
+    /**
+     * 地理坐标圆形
+     */
+    public static class Circle implements Serializable {
+
+        /**
+         * 圆心
+         */
+        private Point center;
+
+        /**
+         * 半径
+         */
+        private double r;
+
+        /**
+         * 构造器
+         *
+         * @param center 圆心
+         * @param r      半径
+         */
+        public Circle(Point center, double r) {
+            this.center = center;
+            this.r = r;
+        }
+
+        public Point getCenter() {
+            return center;
+        }
+
+        public void setCenter(Point center) {
+            this.center = center;
+        }
+
+        public double getR() {
+            return r;
+        }
+
+        public void setR(double r) {
+            this.r = r;
+        }
+
+        /**
+         * 判断点是否在圆形范围内
+         *
+         * @param point 点
+         * @return -1 - 点在圆外, 0 - 点在圆上, 1 - 点在圆内
+         */
+        public int contains(Point point) {
+            double value = Math.hypot((point.longitude - center.longitude), (point.latitude - center.latitude));
+            if (value > r) {
+                // 点在圆外
+                return -1;
+            } else if (value < r) {
+                // 点在圆内
+                return 1;
+            }
+            // 点在圆上
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Circle circle = (Circle) o;
+
+            return new EqualsBuilder()
+                    .append(r, circle.r)
+                    .append(center, circle.center)
+                    .isEquals();
+        }
+
+        @Override
+        public int hashCode() {
+            return new HashCodeBuilder(17, 37)
+                    .append(center)
+                    .append(r)
+                    .toHashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "Circle{" + "center=" + center + ", r=" + r + '}';
+        }
+    }
+
+    /**
+     * 地理坐标多边形区域
+     */
+    public static class Polygon implements Serializable {
+
+        /**
+         * 多边形坐标点列表
+         */
+        private List<Point> points = new ArrayList<Point>();
+
+        public Polygon() {
+        }
+
+        public Polygon(Point[] points) {
+            if (ArrayUtils.isNotEmpty(points)) {
+                this.points.addAll(Arrays.asList(points));
+            }
+        }
+
+        public Polygon(Collection<Point> points) {
+            if (points != null && !points.isEmpty()) {
+                this.points.addAll(points);
+            }
+        }
+
+        public boolean isEmpty() {
+            return this.points.isEmpty();
+        }
+
+        public Polygon add(Point point) {
+            if (point != null) {
+                this.points.add(point);
+            }
+            return this;
+        }
+
+        public Polygon add(double longitude, double latitude) {
+            this.points.add(new Point(longitude, latitude));
+            return this;
+        }
+
+        public List<Point> getPoints() {
+            return points;
+        }
+
+        public boolean in(Point point) {
+            int nCross = 0;
+            for (int i = 0; i < points.size(); i++) {
+                Point p1 = points.get(i);
+                Point p2 = points.get((i + 1) % points.size());
+                if (p1.latitude == p2.latitude) {
+                    continue;
+                }
+                if (point.latitude < Math.min(p1.latitude, p2.latitude)) {
+                    continue;
+                }
+                if (point.latitude >= Math.max(p1.latitude, p2.latitude)) {
+                    continue;
+                }
+                double x = (point.latitude - p1.latitude) * (p2.longitude - p1.longitude) / (p2.latitude - p1.latitude) + p1.longitude;
+                if (x > point.longitude) {
+                    nCross++;
+                }
+            }
+            return (nCross % 2 == 1);
+        }
+
+        public boolean on(Point point) {
+            for (int i = 0; i < points.size(); i++) {
+                Point p1 = points.get(i);
+                Point p2 = points.get((i + 1) % points.size());
+                if (point.latitude < Math.min(p1.latitude, p2.latitude)) {
+                    continue;
+                }
+                if (point.latitude > Math.max(p1.latitude, p2.latitude)) {
+                    continue;
+                }
+                if (p1.latitude == p2.latitude) {
+                    double minX = Math.min(p1.longitude, p2.longitude);
+                    double maxX = Math.max(p1.longitude, p2.longitude);
+                    if ((point.latitude == p1.latitude) && (point.longitude >= minX && point.longitude <= maxX)) {
+                        return true;
+                    }
+                } else {
+                    double x = (point.latitude - p1.latitude) * (p2.longitude - p1.longitude) / (p2.latitude - p1.latitude) + p1.longitude;
+                    if (x == point.longitude) {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
